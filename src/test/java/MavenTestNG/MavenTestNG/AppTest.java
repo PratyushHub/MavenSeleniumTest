8931628fb883eabb7172cab289386955d98d2117
unknown <pratyush.endowment@gmail.com>
2023-11-23 22:32:17 +0530

    second commit

--- src/test/java/MavenTestNG/MavenTestNG/AppTest.java
+++ src/test/java/MavenTestNG/MavenTestNG/AppTest.java
@@ -2,12 +2,14 @@ package MavenTestNG.MavenTestNG;
 
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -19,11 +21,19 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+	@Parameters("Browser")
 	@Test
-	public void startBrowser() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	public void startBrowser(String browsername) {
+		System.out.println("Selected browser-"+browsername);
+		WebDriver driver=null;
+		if(browsername.contains("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}else if(browsername.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+		}
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getTitle());
